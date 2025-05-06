06de6bb621ce8c094b66c3aac62c8e11eebdc83d
vitu <vitorafael.r.c@gmail.com>
2025-05-05 22:35:03 -0300

    📦 Feature: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tour Guide example')
+    .setDescription('The tour-guide API description')
+    .setVersion('1.0')
+    .addTag('tour')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
+
 bootstrap();
